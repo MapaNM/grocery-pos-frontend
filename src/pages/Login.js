3001d53db7f07824087e28fffb1347217fc4900f
MapaNM <nadeeshmapa@gmail.com>
2026-04-09 08:51:20 +0530

    changes in login UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -48,7 +48,7 @@ const Login = () => {
     <div className="login-container">
       <div className="login-card">
         <div className="login-header">
-          <h1>🛒 Grocery POS</h1>
+          <h1>🛒 Mini Mart POS</h1>
           <p>Sign in to your account</p>
         </div>
         
